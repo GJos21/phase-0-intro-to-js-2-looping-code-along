a26c7f57ca55b3627545781d281d8da74c6d3804
Greg Schmitt <gregjschmitt@gmail.com>
2021-11-19 10:47:39 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return newArr;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count)
+        count--;
+    }
+}
+
